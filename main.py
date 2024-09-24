ddc28bf8081a25d4a677fedbca597e65500235ad
chisnikoff <45393985+chisnikoff@users.noreply.github.com>
2024-09-24 23:16:40 +0700

    Сдвинул цветовые каналы и объединил

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from PIL import Image
 
-image = Image.open("monro.jpg")
-red, green, blue = image.split()
-red.save('red.jpg')
-green.save('green.jpg')
-blue.save('blue.jpg')
-shift = 100
-red = Image.open("red.jpg")
-red2 = red
-coordinates = (shift, 0, red.width, red.height)
-coordinates2 = (shift/2, 0, red.width-shift/2, red.height)
-print(coordinates2)
-crop_red = red.crop(coordinates)
-crop_red2 = red2.crop(coordinates2)
-# green = Image.open("green.jpg")
-# blue = Image.open("blue.jpg")
-# new_image = Image.merge("RGB", (red, green, blue))
-# crop_red.save('crop_red.jpg')
-image_all = Image.blend(crop_red, crop_red2, 0.3)
-image_all.save('all.jpg')
-# print(image_all.width)
+shift_value = 50
+img = Image.open("monro.jpg")
+red_channel, green_channel, blue_channel = img.split()
+
+red2 = red_channel
+left_shift_coordinates = (shift_value, 0, red_channel.width, red_channel.height)
+croped_coordinates = (shift_value/2, 0, red_channel.width-shift_value/2, red_channel.height)
+crop_red = red_channel.crop(left_shift_coordinates)
+crop_red2 = red2.crop(croped_coordinates)
+img_red = Image.blend(crop_red, crop_red2, 0.3)
+# img_red.save('red_shift.jpg')
+
+blue2 = blue_channel
+right_shift_coordinates = (0, 0, blue_channel.width-shift_value, blue_channel.height)
+croped_coordinates = (shift_value/2, 0, blue_channel.width-shift_value/2, blue_channel.height)
+crop_blue = blue_channel.crop(right_shift_coordinates)
+crop_blue2 = blue2.crop(croped_coordinates)
+img_blue = Image.blend(crop_blue, crop_blue2, 0.3)
+# img_blue.save('blue_shift.jpg')
+
+croped_coordinates = (shift_value/2, 0, green_channel.width-shift_value/2, green_channel.height)
+img_green = green_channel.crop(croped_coordinates)
+
+new_image = Image.merge("RGB", (img_red, img_green, img_blue))
+new_image.save('all.jpg')
