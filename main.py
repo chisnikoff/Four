2c94524c4fecc3551d9d8e3cf93c4a855a4618c3
chisnikoff <45393985+chisnikoff@users.noreply.github.com>
2024-09-24 23:48:59 +0700

    Добавил уменьшение до аватарки 80х80

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ croped_coordinates = (shift_value/2, 0, red_channel.width-shift_value/2, red_cha
 crop_red = red_channel.crop(left_shift_coordinates)
 crop_red2 = red2.crop(croped_coordinates)
 img_red = Image.blend(crop_red, crop_red2, 0.3)
-# img_red.save('red_shift.jpg')
 
 blue2 = blue_channel
 right_shift_coordinates = (0, 0, blue_channel.width-shift_value, blue_channel.height)
@@ -18,10 +17,11 @@ croped_coordinates = (shift_value/2, 0, blue_channel.width-shift_value/2, blue_c
 crop_blue = blue_channel.crop(right_shift_coordinates)
 crop_blue2 = blue2.crop(croped_coordinates)
 img_blue = Image.blend(crop_blue, crop_blue2, 0.3)
-# img_blue.save('blue_shift.jpg')
 
 croped_coordinates = (shift_value/2, 0, green_channel.width-shift_value/2, green_channel.height)
 img_green = green_channel.crop(croped_coordinates)
 
 new_image = Image.merge("RGB", (img_red, img_green, img_blue))
+new_image.thumbnail((80, 80))
 new_image.save('all.jpg')
+print(new_image.size)
